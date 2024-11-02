42ee235edd42f16a80fda21b03c75b0e4e3d512c
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-11-02 18:22:26 -0300

    Add flex-1 e h-screem

--- projeto-react/src/app/fundamentos/pagina/page.tsx
+++ projeto-react/src/app/fundamentos/pagina/page.tsx
@@ -4,9 +4,10 @@ import Header from "@/components/Header"
 
 export default function Page() {
     return (
+        //h-screen faz com que todo o conteudo da página inteira seja preenchido;
         <div className={`
             flex flex-col gap-4 
-            p-4
+            p-4 h-screen
         `}>
             <Header />
             <Content />
--- projeto-react/src/components/Content.tsx
+++ projeto-react/src/components/Content.tsx
@@ -1,6 +1,6 @@
 export default function Content() {
     return (
-        //O flex-1 faz com que o conteudo ocupe todo o espaço restante;
+        //O flex-1 faz com que o conteudo onde ele é chamado ocupe todo o espaço restante;
         <div className="bg-emerald-500 h-36 flex justify-center items-center flex-1 text-3xl rounded-lg">
             Conteudo
         </div>
